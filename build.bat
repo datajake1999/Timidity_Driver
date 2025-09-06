f96b68842de371f99b621410472dea6907c8df6e
datajake1999 <datajake@gmail.com>
2025-09-06 06:03:26 -0700

    Added Unicode defines to build script.

--- build.bat
+++ build.bat
@@ -2,17 +2,17 @@
 call "C:\Program Files\Microsoft Visual Studio 8\VC\bin\vcvars32.bat"
 call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /SRV32 /RETAIL
 md X86
-cl /nologo -c /I "driver\include" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib winmm.lib /nologo /dll /def:"driver\src\winmm_drv.def" /OUT:X86\timidity.dll
 del *.obj
-cl /nologo -c /O2 installer\drvinst.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 installer\drvinst.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib /nologo /OUT:X86\drvsetup.exe
 del *.obj
-cl /nologo -c /O2 test\test.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 test\test.c
 link *.obj bufferoverflowU.lib kernel32.lib user32.lib winmm.lib /nologo /OUT:X86\test.exe
 del *.obj
 rc config\Timidity.rc
-cl /nologo -c /O2 config\*.c common\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 config\*.c common\*.c
 link dialog.obj registry.obj winmain.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /OUT:X86\timcfg.exe
 link cpl.obj dialog.obj registry.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /dll /def:"config\cpl.def" /OUT:X86\timcfg.cpl
 del *.obj
@@ -23,16 +23,16 @@ del *.exp *.lib
 cd..
 call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /X64 /RETAIL
 md X64
-cl /nologo -c /I "driver\include" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib winmm.lib /nologo /dll /def:"driver\src\winmm_drv.def" /OUT:X64\timidity.dll
 del *.obj
-cl /nologo -c /O2 installer\drvinst.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 installer\drvinst.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib /nologo /OUT:X64\drvsetup.exe
 del *.obj
-cl /nologo -c /O2 test\test.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 test\test.c
 link *.obj bufferoverflowU.lib kernel32.lib user32.lib winmm.lib /nologo /OUT:X64\test.exe
 del *.obj
-cl /nologo -c /O2 config\*.c common\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 config\*.c common\*.c
 link dialog.obj registry.obj winmain.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /OUT:X64\timcfg.exe
 link cpl.obj dialog.obj registry.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /dll /def:"config\cpl.def" /OUT:X64\timcfg.cpl
 del *.obj
@@ -43,16 +43,16 @@ del *.exp *.lib
 cd..
 call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /SRV64 /RETAIL
 md IA64
-cl /nologo -c /I "driver\include" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib winmm.lib /nologo /dll /def:"driver\src\winmm_drv.def" /OUT:IA64\timidity.dll
 del *.obj
-cl /nologo -c /O2 installer\drvinst.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 installer\drvinst.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib /nologo /OUT:IA64\drvsetup.exe
 del *.obj
-cl /nologo -c /O2 test\test.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 test\test.c
 link *.obj bufferoverflowU.lib kernel32.lib user32.lib winmm.lib /nologo /OUT:IA64\test.exe
 del *.obj
-cl /nologo -c /O2 config\*.c common\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 config\*.c common\*.c
 link dialog.obj registry.obj winmain.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /OUT:IA64\timcfg.exe
 link cpl.obj dialog.obj registry.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /dll /def:"config\cpl.def" /OUT:IA64\timcfg.cpl
 del *.obj
