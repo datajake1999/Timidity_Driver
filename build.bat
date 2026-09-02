3661801e6639e284bb63fe7a8a05f6a77b7698f8
datajake1999 <datajake@gmail.com>
2026-09-02 00:34:38 -0700

    Updated synth library.

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 call "C:\Program Files\Microsoft Visual Studio 8\VC\bin\vcvars32.bat"
 call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /SRV32 /RETAIL
 md X86
-cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include" /I "driver\include\msinttypes" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib winmm.lib /nologo /dll /def:"driver\src\winmm_drv.def" /OUT:X86\timidity.dll
 del *.obj
 cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 installer\drvinst.c
@@ -16,7 +16,7 @@ cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 config\*.c common\*.c
 link dialog.obj registry.obj winmain.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /OUT:X86\timcfg.exe
 link cpl.obj dialog.obj registry.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /dll /def:"config\cpl.def" /OUT:X86\timcfg.cpl
 del *.obj
-cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 common\*.c driver\timidity\*.c mid2wav\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include\msinttypes" /O2 common\*.c driver\timidity\*.c mid2wav\*.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib /nologo /OUT:X86\mid2wav.exe
 del *.obj
 copy installer\install.bat X86
@@ -26,7 +26,7 @@ del *.exp *.lib
 cd..
 call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /X64 /RETAIL
 md X64
-cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include" /I "driver\include\msinttypes" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib winmm.lib /nologo /dll /def:"driver\src\winmm_drv.def" /OUT:X64\timidity.dll
 del *.obj
 cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 installer\drvinst.c
@@ -39,7 +39,7 @@ cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 config\*.c common\*.c
 link dialog.obj registry.obj winmain.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /OUT:X64\timcfg.exe
 link cpl.obj dialog.obj registry.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /dll /def:"config\cpl.def" /OUT:X64\timcfg.cpl
 del *.obj
-cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 common\*.c driver\timidity\*.c mid2wav\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include\msinttypes" /O2 common\*.c driver\timidity\*.c mid2wav\*.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib /nologo /OUT:X64\mid2wav.exe
 del *.obj
 copy installer\install.bat X64
@@ -49,7 +49,7 @@ del *.exp *.lib
 cd..
 call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /SRV64 /RETAIL
 md IA64
-cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include" /I "driver\include\msinttypes" /O2 common\*.c driver\src\*.cpp driver\timidity\*.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib winmm.lib /nologo /dll /def:"driver\src\winmm_drv.def" /OUT:IA64\timidity.dll
 del *.obj
 cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 installer\drvinst.c
@@ -62,7 +62,7 @@ cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 config\*.c common\*.c
 link dialog.obj registry.obj winmain.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /OUT:IA64\timcfg.exe
 link cpl.obj dialog.obj registry.obj config\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib kernel32.lib user32.lib /nologo /dll /def:"config\cpl.def" /OUT:IA64\timcfg.cpl
 del *.obj
-cl /nologo -c /D "UNICODE" /D "_UNICODE" /O2 common\*.c driver\timidity\*.c mid2wav\*.c
+cl /nologo -c /D "UNICODE" /D "_UNICODE" /I "driver\include\msinttypes" /O2 common\*.c driver\timidity\*.c mid2wav\*.c
 link *.obj advapi32.lib bufferoverflowU.lib kernel32.lib user32.lib /nologo /OUT:IA64\mid2wav.exe
 del *.obj
 copy installer\install.bat IA64
